e1ea6ba0291a82dda4140c2317f4f4dfd79d99f3
Manish sutradhar <105163926+Manish4403@users.noreply.github.com>
2024-09-20 15:33:30 +0530

    Update Mux4to1.v

--- Sources/Mux4to1.v
+++ Sources/Mux4to1.v
@@ -1,11 +1,11 @@
 `timescale 1ns/1ps
 
 module Mux4to1(in0, in1, in2, in3,sel, out);
-    input [31:0] in0, in1, in2, in3;
+    input signed [31:0] in0, in1, in2, in3;
     input [1:0]sel;
-    output reg[31:0]out;
+    output reg signed [31:0]out;
     
-    always @(sel or in0 or in1 or in2 or in3)begin
+    always @(*)begin
         case(sel)
             2'b00: out = in0;
             2'b01: out = in1;
