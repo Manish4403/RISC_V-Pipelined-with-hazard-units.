98c086850885f3c61d32e0567f1de8aa0ddee94f
Manish sutradhar <105163926+Manish4403@users.noreply.github.com>
2024-09-20 15:30:29 +0530

    Update Controller.v

--- Sources/Controller.v
+++ Sources/Controller.v
@@ -20,10 +20,16 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Controller(clk,rst, op, funct3, funct7, pcsrc, resultsrc, memwrite, alucontrol, alusrc, immsrc, regwrite, load_src, branch,store_src);
+module Controller(
+                  clk,rst, op, funct3, funct7, pcsrc, resultsrc, memwrite, alucontrol, alusrc, 
+                  immsrc, regwrite, load_src, store_src, jalD, jalrD, loadD, storeD,
+                  jalE, jalrE, loadE, storeE, branch
+                  );
+                  
     input [6:0]op;
     input [2:0] funct3;
-    input branch,funct7, clk, rst;
+    input funct7, clk, rst;
+    input jalE, jalrE, loadE, storeE, branch;
     
     output [1:0]pcsrc;
     output reg [1:0] store_src;
@@ -33,7 +39,7 @@ module Controller(clk,rst, op, funct3, funct7, pcsrc, resultsrc, memwrite, aluco
     output memwrite, alusrc, regwrite;
     output reg [2:0] load_src;
     wire  [1:0]aluop;
-    wire jal, jalr, load,store;
+    output jalD, jalrD, loadD, storeD;
     wire [2:0] l_s;
     reg [2:0] l_s_next;
     wire [1:0] s_s;
@@ -43,7 +49,8 @@ module Controller(clk,rst, op, funct3, funct7, pcsrc, resultsrc, memwrite, aluco
     
     // Instatiating the  main decoder
     Main_decoder Main_decoder(.op(op), .resultsrc(resultsrc), .memwrite(memwrite), .alusrc(alusrc), .immsrc(immsrc), .regwrite(regwrite), 
-                              .jal(jal), .jalr(jalr) ,.aluop(aluop), .load(load), .funct3(funct3), .funct7(funct7), .store(store));
+                              .jal(jalD), .jalr(jalrD) ,.aluop(aluop), .load(loadD), .funct3(funct3), .funct7(funct7), .store(storeD)
+                              );
                               
     // Instantiating the branch module
     store_controller store_controller(.funct3(funct3[1:0]),  .result(s_s));
@@ -51,14 +58,17 @@ module Controller(clk,rst, op, funct3, funct7, pcsrc, resultsrc, memwrite, aluco
     //Load controller
     load_controller load_controller(.funct3(funct3),  .result(l_s));
     
-    assign pcsrc = (jalr) ? 2'b10 : (branch | jal) ? 2'b01 : 2'b00;
+//    //Branch controller
+//    Branch_controller Branch_controller(.OPcode(op), .funct3(funct3), .ALUcontrol(alucontrol), .Branch(branch));
+    
+    assign pcsrc = (jalrE) ? 2'b10 : (branch | jalE) ? 2'b01 : 2'b00;
     
     
     // Hold load.
     always@(posedge clk) begin
         if(rst)
             load_src <= 3'b000;
-        else if(load)
+        else if(loadE)
             load_src <= l_s;
         else
             load_src <= l_s_next;
@@ -71,7 +81,7 @@ module Controller(clk,rst, op, funct3, funct7, pcsrc, resultsrc, memwrite, aluco
     always@(posedge clk) begin
         if(rst)
             store_src <= 2'b00;
-        else if(store)
+        else if(storeE)
             store_src <= s_s;
         else
             store_src <= s_s_next;
